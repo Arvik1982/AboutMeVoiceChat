cc0c3fe3d10c4909df78e59bcaa14f68918c4362
Арсений <@email>
2026-05-19 19:25:15 +0300

    fix: letter content

--- server/src/controllers/contactController.ts
+++ server/src/controllers/contactController.ts
@@ -32,7 +32,7 @@ export const sendContactEmail = async (req: Request, res: Response) => {
     // 2. Копия пользователю
     await sendEmail({
       to: email,
-      subject: "Мы получили ваше письмо",
+      subject: `${name}, Мы получили ваше письмо`,
       text: `Привет! ${name || ""},
 
 Спасибо за ваше письмо. Я его получил и в ближайшее время дам на него ответ.
