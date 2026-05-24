8554fbc8b47747e85c8cefdce7ebfd6950f37390
Арсений <@email>
2026-05-24 12:43:08 +0300

    fix:vite config base change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/about-me/",
+  base: "/AboutMeVoiceChat/",
   css: {
     modules: {
       localsConvention: "camelCase",
